861f300b5f4b1bf20b8704594dc22c0db3160ec6
Youssouf SISSOKO <ysissoko78@gmail.com>
2025-08-28 08:40:44 +0000

    added application port to env file

---
+++ import.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker compose exec -T database psql -U postgres -c "SELECT 1 FROM pg_database WHERE datname = 'lacollection_db'" | grep -q 1 || docker compose exec -T database psql -U postgres -c "CREATE DATABASE lacollection_db;" && docker compose exec -T database psql -U postgres -d lacollection_db < data/lacollectiondb.sql
+
